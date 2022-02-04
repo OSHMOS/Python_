4317746d14f6fbe8dd6b77a8e70e24082e293160
OSHMOS <ojames9707@gmail.com>
2022-02-04 23:59:10 +0900

    코딩테스트 : 큰수의법칙 2번 알고리즘은 완 오류 잡는 중

---
+++ Algorithm/CodingTest/큰수의법칙.py
@@ -0,0 +1,18 @@
+N, M, K = map(int, input().split())
+list = []
+result = 0
+cnt = 0
+
+for i in range(N):
+    num = int(input())
+    list.append(num)
+temp = sorted(list, reverse=True)
+
+for i in range(M):
+    result += temp[0]
+    cnt += 1
+    if cnt == K-1:
+        result += temp[1]
+        cnt = 0
+
+print(result)
