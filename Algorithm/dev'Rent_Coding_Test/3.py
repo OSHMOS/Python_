0c2e72829fa0a547e54b21d8f2534397e71b99c1
OSHMOS <ojames9707@gmail.com>
2022-03-10 11:45:42 +0900

    백준 파이썬 입출력 10번 완

--- Algorithm/dev'Rent_Coding_Test/3.py
+++ Algorithm/dev'Rent_Coding_Test/3.py
@@ -1,3 +1,7 @@
-sentences = list(input().split())
+# sentences = list(input().split())
 
-print(len(sentences))
+# print(len(sentences))
+
+import sys
+st=list(map(str,sys.stdin.readline().split()))
+print(len(st))
