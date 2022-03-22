94210daed5e7c5265086b491493c338275eb3ab1
OSHMOS <ojames9707@gmail.com>
2022-03-22 14:46:16 +0900

    코드잇 : 파이썬 중급 시작 (모듈)

---
+++ codeit/modules/area.py
@@ -0,0 +1,10 @@
+PI = 3.14
+
+
+def circle(radius):
+    return PI * radius ** 2
+
+
+def square(length):
+    return length ** 2
+
---
+++ codeit/modules/run.py
@@ -0,0 +1,5 @@
+import area
+
+print(area.circle(3))
+print(area.square(4))
+print(area.PI)
