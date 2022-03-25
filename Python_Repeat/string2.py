d53e11b937eb74b30552c9e6a92d13e7f4f7bf36
OSHMOS <ojames9707@gmail.com>
2022-03-25 14:20:57 +0900

    문자열을 대상으로 하는 유용한 함수들 : len(), .upper(), .lower(), .isupper(), .islower(), .count('?'), .find('?')

---
+++ Python_Repeat/string2.py
@@ -0,0 +1,10 @@
+# 문자열을 대상으로 하는 유용한 함수들에 대해서 배워보자.
+str = "oshmos"
+str2 = "HUN"
+print(len(str))
+print(str.upper())
+print(str2.lower())
+print(str.isupper())
+print(str.islower())
+print(str.count('o'))
+print(str.find('o')) # 두 번째 인자로 '시작 위치'를 지정해줄 수도 있다.
