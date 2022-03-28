f6230ad522bdbde2e3a94df62c222de9c0a81f1f
OSHMOS <ojames9707@gmail.com>
2022-03-28 14:50:09 +0900

    코드잇 : 스크립트 실행 시 모듈의 결과 함께 출력 안 되게 하는 방법

--- codeit/modules/area.py
+++ codeit/modules/area.py
@@ -1,5 +1,6 @@
-PI = 3.14
+print(f'area 모듈 이름 : {__name__}')
 
+PI = 3.14
 
 def circle(radius):
     return PI * radius ** 2
@@ -8,8 +9,8 @@ def circle(radius):
 def square(length):
     return length ** 2
 
-
-print(circle(2) == 12.56)
-print(circle(5) == 78.5)
-print(square(2) == 4)
-print(square(5) == 25)
+if __name__ == '__main__':
+    print(circle(2) == 12.56)
+    print(circle(5) == 78.5)
+    print(square(2) == 4)
+    print(square(5) == 25)
--- codeit/modules/run.py
+++ codeit/modules/run.py
@@ -1,3 +1,4 @@
+print(f'run 스크립트 이름 : {__name__}')
 # 스크립트
 # 프로그램을 작동시키는 코드를 담은 실행 용도의 파일
 
@@ -5,9 +6,12 @@
 # 프로그램에 필요한 변수들이나 함수들을 정의해 놓은 파일
 import area
 
-x = float(input('원의 반지름을 입력해 주세요 : '))
-print(f'반지름이 {x}인 원의 면적은  {area.circle(x)}입니다.\n')
+# x = float(input('원의 반지름을 입력해 주세요 : '))
+# print(f'반지름이 {x}인 원의 면적은  {area.circle(x)}입니다.\n')
 
-y = float(input('정사각형의 변의 길이를 입력해 주세요 : '))
-print(f'반지름이 {y}인 원의 면적은  {area.square(y)}입니다.\n')
+# y = float(input('정사각형의 변의 길이를 입력해 주세요 : '))
+# print(f'반지름이 {y}인 원의 면적은  {area.square(y)}입니다.\n')
+
+print('run 파일 실행됨')
+
 
