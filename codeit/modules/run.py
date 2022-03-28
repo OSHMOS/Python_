46ef696c15f7b92fc04c0bcc9b43f113a6ee1839
OSHMOS <ojames9707@gmail.com>
2022-03-28 14:23:58 +0900

    코드잇 : 모듈 검색 경로 출력과 추가

--- codeit/modules/run.py
+++ codeit/modules/run.py
@@ -1,13 +1,6 @@
-# 권장하는 방법
-from area import circle, square as sq
-
-# 같은 이름의 함수는 마지막에 선언한 것이 정의된다.
-# 같은 네임스페이스로 하지 않는 것이 좋다.
-def square(length):
-    return 4 * length
-
-# 파일의 네임스페이스를 리턴
-print(dir())
-print(square(3))
-print(sq(3))
+import sys
+# 모듈 검색 경로 추가
+sys.path.append('/Users/codeit/Desktop')
+print(sys.path)
+# 항상 현재 폴더가 모듈 검색 경로의 시작
 
