6aae18f0b3c90fe873f094d04c75cc33475eaad9
OSHMOS <ojames9707@gmail.com>
2022-03-28 13:40:49 +0900

    코드잇 : 권장하는 import 방식과 namespace에 대하여

--- codeit/modules/run.py
+++ codeit/modules/run.py
@@ -1,9 +1,13 @@
-# import area
-# import area as ar
-from area import circle, square
+# 권장하는 방법
+from area import circle, square as sq
 
-# print(dir(area))
-#
+# 같은 이름의 함수는 마지막에 선언한 것이 정의된다.
+# 같은 네임스페이스로 하지 않는 것이 좋다.
+def square(length):
+    return 4 * length
+
+# 파일의 네임스페이스를 리턴
 print(dir())
+print(square(3))
+print(sq(3))
 
-# __~__ 던더 특수 변수
