fe34699d6e3beef8a08f699ece7d7780898c7356
OSHMOS <ojames9707@gmail.com>
2022-03-28 14:31:19 +0900

    코드잇 : 모듈과 스크립트

--- codeit/modules/run.py
+++ codeit/modules/run.py
@@ -1,6 +1,13 @@
-import sys
-# 모듈 검색 경로 추가
-sys.path.append('/Users/codeit/Desktop')
-print(sys.path)
-# 항상 현재 폴더가 모듈 검색 경로의 시작
+# 스크립트
+# 프로그램을 작동시키는 코드를 담은 실행 용도의 파일
+
+# 모듈
+# 프로그램에 필요한 변수들이나 함수들을 정의해 놓은 파일
+import area
+
+x = float(input('원의 반지름을 입력해 주세요 : '))
+print(f'반지름이 {x}인 원의 면적은  {area.circle(x)}입니다.\n')
+
+y = float(input('정사각형의 변의 길이를 입력해 주세요 : '))
+print(f'반지름이 {y}인 원의 면적은  {area.square(y)}입니다.\n')
 
