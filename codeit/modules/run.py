de6e20972cb08032237208c9f5b0a1184aa5fe79
OSHMOS <ojames9707@gmail.com>
2022-03-28 13:34:57 +0900

    코드잇 : dir, 내장 함수 확인하기

--- codeit/modules/run.py
+++ codeit/modules/run.py
@@ -1,20 +1,9 @@
-# from area import circle, square
+# import area
 # import area as ar
-# from area import square as sq
+from area import circle, square
 
-from area import *
+# print(dir(area))
+#
+print(dir())
 
-from shapes2d import Circle as c
-from shapes2d import Square as s
-
-# print(ar.circle(3))
-# print(ar.sq(4))
-
-# print(circle(3))
-# print(square(4))
-
-sq = s(3)
-print(sq.area())
-
-cr = c(2)
-print(cr.area())
+# __~__ 던더 특수 변수
