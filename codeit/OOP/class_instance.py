8495ef4ec3a2765f32cbb89a4112a78cebf57160
OSHMOS <ojames9707@gmail.com>
2022-03-29 13:57:03 +0900

    코드잇 : 인스턴스 메소드의 첫 번째 파라미터는 꼭 self!

--- codeit/OOP/class_instance.py
+++ codeit/OOP/class_instance.py
@@ -1,11 +1,12 @@
 # 클래스
+# 인스턴스 메소드의 첫 번째 파라미터는 꼭 self로 쓰는 것을 권장함
 class User:
-    def say_hello(some_user):
+    def say_hello(self):
         # 인사 메세지 출력 메소드
-        print(f"안녕하세요! 저는 {some_user.name}입니다!")
+        print(f"안녕하세요! 저는 {self.name}입니다!")
 
-    def login(some_user, my_email, my_password):
-        if some_user.email == my_email and some_user.password == my_password:
+    def login(self, my_email, my_password):
+        if self.email == my_email and self.password == my_password:
             print("로그인 성공, 환영합니다.")
         else:
             print("로그인 실패, 없는 아이디이거나 잘못된 비밀번호입니다.")
