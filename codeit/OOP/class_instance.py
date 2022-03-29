d982a0dfea0ac44eadc0f672f7d5338fbb3b4071
OSHMOS <ojames9707@gmail.com>
2022-03-29 14:18:57 +0900

    코드잇 : 인스턴스 변수 자동 추가하는 인스턴스 메소드

--- codeit/OOP/class_instance.py
+++ codeit/OOP/class_instance.py
@@ -1,6 +1,11 @@
 # 클래스
 # 인스턴스 메소드의 첫 번째 파라미터는 꼭 self로 쓰는 것을 권장함
 class User:
+    def initialize(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+    
     def say_hello(self):
         # 인사 메세지 출력 메소드
         print(f"안녕하세요! 저는 {self.name}입니다!")
@@ -14,19 +19,22 @@ class User:
     def check_name(self, name):
         # 파라미터로 받는 name이 유저의 이름과 같은지 불 값으로 리턴하는 메소드
         return self.name == name
+        
 # 객체 (인스턴스)
 user1 = User()
+user1.initialize("Young", "young@codeit.kr", "123456")
+
 user2 = User()
+user2.initialize("Yoonsoo", "yoonsoo@codeit.kr", "abcdef")
 
-# 인스턴스 변수 (속성) 추가하기
-user1.name = "김대위"
-user1.email = "captain@codeit.kr"
-user1.pw = "12345"
+user3 = User()
+user3.initialize("Taeho", "taeho@codeit.kr", "123abc")
 
-user2.name = "강영훈"
-user2.email = "younghoon@codeit.kr"
-user2.pw = "98765"
+user4 = User()
+user4.initialize("Lisa", "list@codeit.kr", "abc123")
 
 # 출력하기
-print(user1.check_name("김대위"))
-print(user1.check_name("강영훈"))
+print(user1.email)
+print(user2.name)
+print(user3.password)
+print(user4.email)
