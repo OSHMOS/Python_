cdd8683c13ad170489101a393ca89a8943203d1a
oshmos <ojames9707@gmail.com>
2022-03-29 22:44:20 +0900

    그리디 : 큰 수의 법칙 코드 리뷰

--- Algorithm/CodingTest/큰 수의 법칙/큰 수의 법칙 단순 답안 예시.py
+++ Algorithm/CodingTest/큰 수의 법칙/큰 수의 법칙 단순 답안 예시.py
@@ -1,8 +1,11 @@
+import time
+
 # N, M, K를 공백으로 구분하여 입력받기
 n, m, k = map(int, input().split())
 # N개의 수를 공백으로 구분하여 입력받기
 data = list(map(int, input().split()))
 
+start_time = time.time()
 data.sort() # 입력받은 수들 정렬하기
 first = data[n - 1] # 가장 큰 수
 second = data[n - 2] # 두 번째로 큰 수
@@ -21,3 +24,6 @@ while True:
   m -= 1 # 더할 때마다 1씩 빼기
 
 print(result) # 최종 답안 출력
+
+end_time = time.time()
+print(f"time : {end_time - start_time}")
--- Algorithm/CodingTest/큰 수의 법칙/큰수의법칙 틀린 처음 풀이.py
+++ Algorithm/CodingTest/큰 수의 법칙/큰수의법칙 틀린 처음 풀이.py
@@ -1,19 +1,26 @@
 # 앞으로 주석 쓰는 법을 다시 들여야겠다.
 # 코드 내용이 아무것도 기억이 안난다.
+import time
+
 N, M, K = map(int, input().split())
 result = 0
 cnt = 0
 
+
 for i in range(N):
-    num = int(input())
+    num = input()
     list.append(num)
 temp = sorted(list, reverse=True)
 
+start_time = time.time()
 for i in range(M):
     if cnt == K-1:
-        result += temp[1]
+        result += int(temp[1])
         cnt = 0
     else:
-        result += temp[0]
+        result += int(temp[0])
         cnt += 1
 print(result)
+
+end_time = time.time()
+print(f"time : {end_time - start_time}")
