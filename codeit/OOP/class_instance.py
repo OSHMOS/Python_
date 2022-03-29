f8c524f797bb377290b69f3d741d5e4089940976
OSHMOS <ojames9707@gmail.com>
2022-03-29 14:02:23 +0900

    코드잇 : 인스턴스 변수와 같은 이름의 파라미터를 가져도 된다.

--- codeit/OOP/class_instance.py
+++ codeit/OOP/class_instance.py
@@ -11,17 +11,22 @@ class User:
         else:
             print("로그인 실패, 없는 아이디이거나 잘못된 비밀번호입니다.")
 
+    def check_name(self, name):
+        # 파라미터로 받는 name이 유저의 이름과 같은지 불 값으로 리턴하는 메소드
+        return self.name == name
 # 객체 (인스턴스)
 user1 = User()
 user2 = User()
-user3 = User()
 
 # 인스턴스 변수 (속성) 추가하기
 user1.name = "김대위"
 user1.email = "captain@codeit.kr"
-user1.password = "12345"
+user1.pw = "12345"
+
+user2.name = "강영훈"
+user2.email = "younghoon@codeit.kr"
+user2.pw = "98765"
 
 # 출력하기
-User.say_hello(user1)
-user1.say_hello() # 파라미터를 따로 써 줄 필요가 없는 형태
-user1.login("captain@codeit.kr", "12345")
+print(user1.check_name("김대위"))
+print(user1.check_name("강영훈"))
