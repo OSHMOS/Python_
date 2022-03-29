7fa2028db2dcbf00b9407fa0a5c88cbe77d5b065
OSHMOS <ojames9707@gmail.com>
2022-03-29 14:37:43 +0900

    코드잇 : 인스턴스 출력 시 원하는 내용 출력할 수 있는 메소드 __str__

--- codeit/OOP/class_instance.py
+++ codeit/OOP/class_instance.py
@@ -13,27 +13,14 @@ class User:
         # 인사 메세지 출력 메소드
         print(f"안녕하세요! 저는 {self.name}입니다!")
 
-    def login(self, my_email, my_password):
-        if self.email == my_email and self.password == my_password:
-            print("로그인 성공, 환영합니다.")
-        else:
-            print("로그인 실패, 없는 아이디이거나 잘못된 비밀번호입니다.")
-
-    def check_name(self, name):
-        # 파라미터로 받는 name이 유저의 이름과 같은지 불 값으로 리턴하는 메소드
-        return self.name == name
+    # double underscore : dunder
+    def __str__(self):
+        return f"사용자 : {self.name}, 이메일 : {self.email}, 비밀번호 : ******"
 
 # 객체 (인스턴스)
 user1 = User("Young", "young@codeit.kr", "123456")
-
 user2 = User("Yoonsoo", "yoonsoo@codeit.kr", "abcdef")
 
-user3 = User("Taeho", "taeho@codeit.kr", "123abc")
-
-user4 = User("Lisa", "list@codeit.kr", "abc123")
-
 # 출력하기
-print(user1.email)
-print(user2.name)
-print(user3.password)
-print(user4.email)
+print(user1)
+print(user2)
