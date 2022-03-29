fee2fe69a66d83af899eb2b7f32759aa73246a40
OSHMOS <ojames9707@gmail.com>
2022-03-29 13:46:10 +0900

    코드잇 : 인스턴스 메소드 선언

--- codeit/OOP/class_instance.py
+++ codeit/OOP/class_instance.py
@@ -1,13 +1,15 @@
 # 클래스
 class User:
-    pass
+    def say_hello(some_user):
+        # 인사 메세지 출력 메소드
+        print(f"안녕하세요! 저는 {some_user.name}입니다!")
 
 # 객체 (인스턴스)
 user1 = User()
 user2 = User()
 user3 = User()
 
-# 인스턴스 변수 추가하기
+# 인스턴스 변수 (속성) 추가하기
 user1.name = "김대위"
 user1.email = "captain@codeit.kr"
 user1.password = "12345"
@@ -26,3 +28,6 @@ print(user2.password)
 # 정의하지 않은 인스턴스 변수는 사용할 수 없다.
 # 인스턴스 변수를 사용하려면 미리 정의해야 한다.
 # print(user1.age)
+User.say_hello(user1)
+User.say_hello(user2)
+User.say_hello(user3)
