063a069c359d4810ae2253f90b20b7c6bf8226ee
OSHMOS <ojames9707@gmail.com>
2022-03-30 11:49:33 +0900

    코드잇 : 클래스 변수의 값을 설정할 때는 꼭 클래스 변수 이름으로만 설정해야 함

--- codeit/OOP/oop.py
+++ codeit/OOP/oop.py
@@ -14,4 +14,9 @@ user1 = User("강영훈", "younghoon@codeit.kr", "123456")
 user2 = User("이윤수", "yoonsoo@codeit.kr", "abcdef")
 user3 = User("서혜린", "lisa@codeit.kr", "123abc")
 
+User.count = 5
+
 print(User.count)
+print(user1.count)
+print(user2.count)
+print(user3.count)
