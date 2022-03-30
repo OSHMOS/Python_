ea967141794cb6228ba601c059a24d41f82a998b
OSHMOS <ojames9707@gmail.com>
2022-03-30 11:45:59 +0900

    코드잇 : 인스턴스가 공통적으로 가지는 변수, 클래스 변수

---
+++ codeit/OOP/oop.py
@@ -0,0 +1,17 @@
+class User:
+    # 클래스 변수
+    count = 0
+
+    def __init__(self, name, email, pw):
+        # 유저 인스턴스의 모든 변수를 지정해주는 메소드
+        self.name = name
+        self.email = email
+        self.pw = pw
+
+        User.count += 1
+
+user1 = User("강영훈", "younghoon@codeit.kr", "123456")
+user2 = User("이윤수", "yoonsoo@codeit.kr", "abcdef")
+user3 = User("서혜린", "lisa@codeit.kr", "123abc")
+
+print(User.count)
