d1e88aa18173422377b493f17fa21b28a67a3e74
OSHMOS <ojames9707@gmail.com>
2022-03-30 12:06:23 +0900

    코드잇 : 함수를 받아서 꾸며주는 함수, 데코레이터

--- codeit/OOP/oop.py
+++ codeit/OOP/oop.py
@@ -1,22 +1,14 @@
-class User:
-    # 클래스 변수
-    count = 0
+def print_hello():
+    print('안녕하세요!')
 
-    def __init__(self, name, email, pw):
-        # 유저 인스턴스의 모든 변수를 지정해주는 메소드
-        self.name = name
-        self.email = email
-        self.pw = pw
+# 데코레이터 : 어떤 함수를 파라미터로 받아서 꾸며준 다음에 리턴해준다.
+def add_print_to(original):
+    def wrapper():
+        print('함수 시작')
+        original()
+        print('함수 끝')
+    return wrapper
 
-        User.count += 1
+print_hello = add_print_to(print_hello)
 
-user1 = User("강영훈", "younghoon@codeit.kr", "123456")
-user2 = User("이윤수", "yoonsoo@codeit.kr", "abcdef")
-user3 = User("서혜린", "lisa@codeit.kr", "123abc")
-
-User.count = 5
-
-print(User.count)
-print(user1.count)
-print(user2.count)
-print(user3.count)
+print_hello()
