db697e594edaf65f550cdaf4463879333276b89f
OSHMOS <ojames9707@gmail.com>
2022-03-30 12:24:14 +0900

    코드잇 : 클래스 메소드는 인스턴스 변수 사용할 수 없고, 인스턴스 없이도 클래스 변수를 출력해야 한다면 클래스 메소드로 작성해야한다.

--- codeit/OOP/oop.py
+++ codeit/OOP/oop.py
@@ -14,15 +14,10 @@ class User:
     def __str__(self):
         return f"사용자 : {self.name}, 이메일 : {self.email}, 비밀번호 : ******"
 
-    # 클래스 메소드
+    # 클래스 메소드 (인스턴스 변수를 사용하지 않기 때문)
     @classmethod
     def number_of_users(cls):
         print(f"총 유저 수는 : {cls.count}")
 
-user1 = User("강영훈", "younghoon@codeit.kr", "123456")
-user1 = User("이윤수", "yoonsoo@codeit.kr", "abcdef")
-user1 = User("서혜린", "lisa@codeit.kr", "123abc")
-
 # 첫 번째 파라미터로 클래스가 자동 전달
 User.number_of_users()
-user1.number_of_users()
