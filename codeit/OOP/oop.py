19318d1d244a35fcd7e5350e3d9f28f56208fe7b
OSHMOS <ojames9707@gmail.com>
2022-03-30 12:08:44 +0900

    코드잇 : 꾸며주고 싶은 함수에 데코레이터 적용하기

--- codeit/OOP/oop.py
+++ codeit/OOP/oop.py
@@ -1,6 +1,3 @@
-def print_hello():
-    print('안녕하세요!')
-
 # 데코레이터 : 어떤 함수를 파라미터로 받아서 꾸며준 다음에 리턴해준다.
 def add_print_to(original):
     def wrapper():
@@ -9,6 +6,9 @@ def add_print_to(original):
         print('함수 끝')
     return wrapper
 
-print_hello = add_print_to(print_hello)
+@add_print_to
+def print_hello():
+    print('안녕하세요!')
+
 
 print_hello()
