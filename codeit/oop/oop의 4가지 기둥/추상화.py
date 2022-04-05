30dca28fde8a748f65f45bfcb8e8da7a6e7673ce
OSHMOS <ojames9707@gmail.com>
2022-04-05 14:01:43 +0900

    코드잇 : 추상화의 종류, 함수 정의 및 사용

--- codeit/oop/oop의 4가지 기둥/추상화.py
+++ codeit/oop/oop의 4가지 기둥/추상화.py
@@ -1,10 +1,12 @@
 # 지금까지 우리는 추상화를 사용하고 있었다.
 # 변수 선언 및 사용, 함수 정의 및 사용, 클래스 사용 모두 추상화이다.
 
-# 우사인 볼트의 평균 속도 = 10.438413361 m/s
-bolt_speed = 10.438413361
-minute = 60
+# 함수 추상화
+def welcome(name):
+    print("Hello, " + name)
+    print("Welcome to Codeit!")
 
-print(bolt_speed * minute) # 1분(60초) 동안 간 거리
-print(bolt_speed * 2 * minute) # 2분(120초) 동안 간 거리
-print(bolt_speed * 3 * minute) # 3분(120초) 동안 간 거리
+
+welcome("영훈")
+welcome("규식")
+welcome("대위")
