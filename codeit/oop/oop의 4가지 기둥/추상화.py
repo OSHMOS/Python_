b8fbcf1c224d4f01a80441274f77c76b4fd74382
OSHMOS <ojames9707@gmail.com>
2022-04-05 14:03:47 +0900

    코드잇 : 추상화의 종류, 클래스로 인스턴스 정의 및 사용

--- codeit/oop/oop의 4가지 기둥/추상화.py
+++ codeit/oop/oop의 4가지 기둥/추상화.py
@@ -1,12 +1,11 @@
 # 지금까지 우리는 추상화를 사용하고 있었다.
 # 변수 선언 및 사용, 함수 정의 및 사용, 클래스 사용 모두 추상화이다.
 
-# 함수 추상화
-def welcome(name):
-    print("Hello, " + name)
-    print("Welcome to Codeit!")
-
-
-welcome("영훈")
-welcome("규식")
-welcome("대위")
+# [](리스트)를 이용해서 빈 리스트 인스턴스를 정의한다.
+example_list = []
+example_list.append(3)
+example_list.append(2)
+example_list.append(6)
+print(example_list[0])
+print(example_list[1])
+print(example_list[2])
