b50e8a502c619d8c92d2fe686613359404c5ed50
OSHMOS <ojames9707@gmail.com>
2022-04-05 14:14:16 +0900

    코드잇 : 추상화의 좋은 예시, 이름을 통해서 직관적으로 어디에 쓰는 클래스이고 어떻게 사용할지 알 수 있다.

--- codeit/oop/oop의 4가지 기둥/추상화.py
+++ codeit/oop/oop의 4가지 기둥/추상화.py
@@ -1,19 +1,35 @@
-# 추상화의 안 좋은 예시
-class SomeClass:
-    class_variable = 0.02
+# 추상화의 좋은 예시
+class BankAccount:
+    interest = 0.02
 
-    def __init__(self, variable_1, variable_2):
-        self.variable_1 = variable_1
-        self.variable_2 = variable_2
+    def __init__(self, owner_name, balance):
+        self.owner_name = owner_name
+        self.balance = balance
 
-    def method_1(self, some_value):
-        self.variable_2 += some_value
+    def deposit(self, amount):
+        self.balance += amount
 
-    def method_2(self, some_value):
-        if self.variable_2 < some_value:
+    def withdraw(self, amount):
+        if self.balance <  amount:
             print("Insufficient balance!")
         else:
-            self.variable_2 -= some_value
+            self.balance -= amount
 
-    def method_3(self):
-        self.variable_2 *= 1 + SomeClass.class_variable
+    def add_interest(self):
+        self.balance *= 1 + BankAccount.interest
+
+# 어디에 쓰는 클래스이고 어떻게 사용할지 직관적으로 알 수 있다.
+
+
+example_account = BankAccount("oshmos", 1000)
+
+example_account.add_interest()
+print(example_account.balance)
+
+example_account.deposit(500)
+print(example_account.balance)
+
+example_account.withdraw(2000)
+example_account.withdraw(1000)
+
+print(example_account.balance)
