491a2bb8ec89d1f82efbe368af6a2840f2cacfcf
OSHMOS <ojames9707@gmail.com>
2022-04-06 11:12:11 +0900

    코드잇 : setter 함수를 활용하여 음수가 나이로 설정되지 않게 만들기

--- codeit/oop/oop의 4가지 기둥/캡슐화/citizen.py
+++ codeit/oop/oop의 4가지 기둥/캡슐화/citizen.py
@@ -11,7 +11,7 @@ class Citizen:
     def __init__(self, name, age, resident_id):
         '''이름, 나이, 주민등록번호'''
         self.name = name
-        self.__age = age
+        self.set_age(age)
         self.__resident_id = resident_id
 
     def authenticate(self, id_field):
@@ -28,14 +28,22 @@ class Citizen:
 
     def set_age(self, value):
         '''숨겨 놓은 인스턴스 변수 __age의 값을 설정하는 메소드'''
-        self.__age = value
+        if value < 0:
+            print('나이는 0보다 작을 수 없습니다. 기본 값 0으로 나이를 설정하겠습니다.')
+            self.__age = 0
+        else:
+            self.__age = value
 
     def __str__(self):
         '''주민 정보를 문자열로 리턴하는 메소드'''
         return f'{self.name}씨는 {str(self.__age)}살 입니다!'
 
 
-osh = Citizen('oshmos', 26, '12345678')
+osh = Citizen('oshmos', -26, '12345678')
 print(osh.get_age())
-osh.set_age(25)
+
+osh.set_age(-10)
+print(osh.get_age())
+
+osh.set_age(26)
 print(osh.get_age())
