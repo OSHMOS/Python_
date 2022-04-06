1bd84951c17f886597778f95a08875c032551da3
OSHMOS <ojames9707@gmail.com>
2022-04-06 10:32:32 +0900

    코드잇 : 캡슐화, 객체 내부를 숨기는 법 => 클래스의 변수나 메소드에 언더바(_) 2개를 붙여서 외부에서 접근하지 못하게 할 수 있다.

---
+++ codeit/oop/oop의 4가지 기둥/캡슐화/citizen.py
@@ -0,0 +1,31 @@
+'''
+클래스의 변수나 메소드에
+언더바(_) 2개를 붙여서
+외부에서 접근하지 못하게 할 수 있다.
+'''
+class Citizen:
+    '''주민 클래스'''
+    drinking_age = 19 # 음주 가능 나이
+
+    def __init__(self, name, age, resident_id):
+        '''이름, 나이, 주민등록번호'''
+        self.name = name
+        self.__age = age
+        self.__resident_id = resident_id
+
+    def authenticate(self, id_field):
+        '''본인이 맞는지 확인하는 메소드'''
+        return self.__resident_id == id_field
+
+    def can_drink(self):
+        '''음주 가능 나이인지 확인하는 메소드'''
+        return self.__age >= Citizen.drinking_age
+
+    def __str__(self):
+        '''주민 정보를 문자열로 리턴하는 메소드'''
+        return f'{self.name}씨는 {str(self.__age)}살 입니다!'
+
+
+kyusik = Citizen('최규식', 25, '12345678')
+print(kyusik.__age)
+print(kyusik.__resident_id)
