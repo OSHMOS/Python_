c47b09a2dc021f1a419ecb42dc9da784bb63f8c6
OSHMOS <ojames9707@gmail.com>
2022-04-06 11:34:03 +0900

    코드잇 : 코드를 짜다가 중간에 캡슐화를 적용하고 싶을 때는 property 데코레이터를 사용하면 된다.

--- codeit/oop/oop의 4가지 기둥/캡슐화/citizen.py
+++ codeit/oop/oop의 4가지 기둥/캡슐화/citizen.py
@@ -1,10 +1,10 @@
 '''
-캡슐화 정리
-1. 클래스 밖에서 접근 못하게 할 변수, 메소드 정하기
-2. 변수나 메소드 이름 앞에 언더바 2개 붙이기 => 1개만 붙여도 된다.
-2-1. 1개만 붙이는 것은 사실 아무 기능이 없지만 (외부에서 접근 가능하지만)
-     파이썬 개발자들만의 문화이기 때문에 꼭 경고를 지켜야한다.
-3. 변수에 간접 접근할 수 있게 메소드 추가하기 (getter/setter) or 다른 용도의 메소드
+property 데코레이터 함수를 사용할 때
+처음 코드를 짤 때, 캡슐화를 고려하지 않다가
+추후에 캡슐화를 적용하고 싶을 때,
+캡슐화를 적용하고 싶은 변수 이름으로 함수를 만들고
+property 데코레이터를 그 함수 위에 표현해주면,
+getter 함수로 사용 가능하고, 마찬가지로 setter 함수도 만들 수 있다.
 '''
 class Citizen:
     '''주민 클래스'''
@@ -13,23 +13,27 @@ class Citizen:
     def __init__(self, name, age, resident_id):
         '''이름, 나이, 주민등록번호'''
         self.name = name
-        self._age = age
-        self._resident_id = resident_id
+        self.age = age
+        self.resident_id = resident_id
 
     def authenticate(self, id_field):
         '''본인이 맞는지 확인하는 메소드'''
-        return self._resident_id == id_field
+        return self.resident_id == id_field
 
     def can_drink(self):
         '''음주 가능 나이인지 확인하는 메소드'''
-        return self._age >= Citizen.drinking_age
+        return self.age >= Citizen.drinking_age
 
-    def get_age(self):
-        '''숨겨 놓은 인스턴스 변수 __age의 값을 받아오는 메소드'''
+    # 원래 변수 이름이 붙여진 getter 함수를 만들면 된다.
+    @property
+    def age(self):
+        print('나이를 리턴합니다.')
         return self._age
 
-    def set_age(self, value):
-        '''숨겨 놓은 인스턴스 변수 __age의 값을 설정하는 메소드'''
+    # 이거는 setter 함수이다.
+    @age.setter
+    def age(self, value):
+        print('나이를 설정합니다.')
         if value < 0:
             print('나이는 0보다 작을 수 없습니다. 기본 값 0으로 나이를 설정하겠습니다.')
             self._age = 0
@@ -38,10 +42,11 @@ class Citizen:
 
     def __str__(self):
         '''주민 정보를 문자열로 리턴하는 메소드'''
-        return f'{self.name}씨는 {str(self._age)}살 입니다!'
+        return f'{self.name}씨는 {str(self.age)}살 입니다!'
 
 
 osh = Citizen('oshmos', 26, '12345678')
-print(osh._age)
-print(osh._resident_id)
+print(osh.age)
+osh.age = 30
+print(osh.age)
 
