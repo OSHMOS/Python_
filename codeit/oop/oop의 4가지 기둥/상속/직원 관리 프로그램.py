5265e06488b31690f7e5864c4fe0fce78fe65661
OSHMOS <ojames9707@gmail.com>
2022-04-07 14:55:06 +0900

    코드잇 : 직원 별 클래스에 기능 추가하기

--- codeit/oop/oop의 4가지 기둥/상속/직원 관리 프로그램.py
+++ codeit/oop/oop의 4가지 기둥/상속/직원 관리 프로그램.py
@@ -17,16 +17,74 @@ class Employee:
         return f'{Employee.company_name} 직원: {self.name}'
 
 class Cashier(Employee):
+    '''계산대 직원 클래스'''
+    burger_price = 4000
     raise_percentage = 1.05
 
     def __init__(self, name, wage, number_sold):
         super().__init__(name, wage)
         self.number_sold = number_sold
 
+    def take_order(self, money_received):
+        '''주문과 돈을 받고 거스름돈을 리턴한다'''
+        if Cashier.burger_price > money_received:
+            print('돈이 충분하지 않습니다.')
+            return money_received
+        else:
+            self.number_sold += 1
+            change = money_received - Cashier.burger_price
+            return change
+
     def __str__(self):
         return f'{Cashier.company_name} 계산대 직원: {self.name}'
 
 class DeliveryMan(Employee):
-    pass
+    '''배달원 클래스'''
+    burger_price = 4000
+    raise_percentage = 1.03
 
-print(Cashier.mro())
+    def __init__(self, name, wage, on_standby):
+        super().__init__(name, wage)
+        self.on_standby = on_standby
+
+    def deliver(self, address):
+        '''배달원이 대기 중이면 주어진 주소로 배달을 보내고 아니면 설명 메시지를 출력한다'''
+        if self.on_standby:
+            print(f'{address}로 배달 나갑니다!')
+            self.on_standby = False
+        else:
+            print('이미 배달하러 나갔습니다!')
+
+    def back(self):
+        '''배달원을 복귀 처리한다'''
+        self.on_standby = True
+
+    def __str__(self):
+        return f'{DeliveryMan.company_name} 배달원: {self.name}'
+
+jiwoong = Cashier('최지웅', 8900, 0)
+jiwoong.raise_pay()
+print(jiwoong.wage)
+
+print(jiwoong.take_order(7000))
+print(jiwoong.take_order(3000))
+
+print(jiwoong.burger_price)
+print(Cashier.burger_price)
+
+print(jiwoong.number_sold)
+
+print(jiwoong)
+
+taeho = DeliveryMan('성태호', 7000, True)
+
+taeho.raise_pay()
+print(taeho.wage)
+
+taeho.deliver('서울시 코드잇로 51 최고 건물 401호')
+taeho.deliver('서울시 코드잇로 51 최고 건물 402호')
+
+taeho.back()
+taeho.deliver('서울시 코드잇로 51 최고 건물 402호')
+
+print(taeho)
