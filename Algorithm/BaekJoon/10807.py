6ec3393c9b81e2bb69fe2cd0291d58dc6c271f0c
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2022-11-11 12:35:13 +0900

    11.11 algorithm-baekjoon

---
+++ Algorithm/BaekJoon/10807.py
@@ -0,0 +1,4 @@
+N = int(input())
+input_list = list(map(int, input().split()))
+v = int(input())
+print(input_list.count(v))
---
+++ Algorithm/BaekJoon/9086.py
@@ -0,0 +1,4 @@
+T = int(input())
+for _ in range(T):
+  input_list = list(input())
+  print(input_list[0] + input_list[-1])
