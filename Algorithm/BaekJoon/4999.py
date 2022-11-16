16b890b1479aea5c7d13f75c707d06c87b9359cc
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2022-11-16 11:26:33 +0900

    11.16 Algorithm-BaekJoon

---
+++ Algorithm/BaekJoon/4999.py
@@ -0,0 +1,6 @@
+j_max = input()
+doc_want = input()
+if len(j_max) >= len(doc_want):
+    print('go')
+else:
+    print('no')
---
+++ Algorithm/BaekJoon/8370.py
@@ -0,0 +1,2 @@
+int_num = list(map(int, input().split()))
+print(int_num[0] * int_num[1] + int_num[2] * int_num[3])
