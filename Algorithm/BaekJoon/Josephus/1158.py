97e551c5a169113842d32e7b219201535dab4be4
OSHMOS <87176797+OSHMOS@users.noreply.github.com>
2022-12-25 14:24:42 +0900

    12.25 Algorithm-BaekJoon

---
+++ Algorithm/BaekJoon/Josephus/1158.py
@@ -0,0 +1,16 @@
+from sys import stdin
+from collections import deque
+n, k = map(int, stdin.readlin().split())
+s = deque([])
+for i in range(1, n + 1):
+    s.append(i)
+# print(s)
+print('<', end='')
+while s:
+    for _ in range(k - 1):
+        s.append(s[0])
+        s.popleft()
+    print(s.popleft(), end='')
+    if s:
+        print(', ', end='')
+print('>')
